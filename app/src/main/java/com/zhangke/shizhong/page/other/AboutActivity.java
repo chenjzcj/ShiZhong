982897247b8ec98d446d0e4a4da40a4ec3027a14
527633405@qq.com <gh@nihao777>
2019-08-13 10:21:04 +0800

    add

--- app/src/main/java/com/zhangke/shizhong/page/other/AboutActivity.java
+++ app/src/main/java/com/zhangke/shizhong/page/other/AboutActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.blankj.utilcode.util.AppUtils;
 import com.zhangke.shizhong.BuildConfig;
 import com.zhangke.shizhong.R;
 
@@ -19,11 +20,8 @@ import me.drakeet.multitype.Items;
 import me.drakeet.support.about.AbsAboutActivity;
 import me.drakeet.support.about.Card;
 import me.drakeet.support.about.Category;
-import me.drakeet.support.about.Contributor;
-import me.drakeet.support.about.License;
 import me.drakeet.support.about.OnRecommendedClickedListener;
 import me.drakeet.support.about.Recommended;
-import me.drakeet.support.about.extension.RecommendedLoaderDelegate;
 import me.drakeet.support.about.provided.PicassoImageLoader;
 
 /**
@@ -43,27 +41,27 @@ public class AboutActivity extends AbsAboutActivity implements OnRecommendedClic
     @Override
     protected void onCreateHeader(@NonNull ImageView icon, @NonNull TextView slogan, @NonNull TextView version) {
         icon.setImageResource(R.mipmap.round_logo);
-        slogan.setText("不忘初心，方得始终。");
-        version.setText("v " + BuildConfig.VERSION_NAME);
+        //slogan.setText("");
+        version.setText("版本 v " + BuildConfig.VERSION_NAME);
     }
 
     @Override
     protected void onItemsCreated(@NonNull Items items) {
-        RecommendedLoaderDelegate.attach(this, items.size());
+        //RecommendedLoaderDelegate.attach(this, items.size());
 
         items.add(new Category("About"));
-        items.add(new Card(getString(R.string.introduce)));
+        items.add(new Card(getString(R.string.introduce, AppUtils.getAppName())));
 
-        items.add(new Category("Developers"));
-        items.add(new Contributor(R.mipmap.mine_icon, "张可", "Developer & Designer", "https://github.com/0xZhangKe"));
+        //items.add(new Category("Developers"));
+        //items.add(new Contributor(R.mipmap.mine_icon, "张可", "Developer & Designer", "https://github.com/0xZhangKe"));
 
-        items.add(new Category("Open Source Licenses"));
-        items.add(new License("ShiZhong", "ZhangKe", License.APACHE_2, "https://github.com/0xZhangKe/ShiZhong"));
+        //items.add(new Category("Open Source Licenses"));
+        //items.add(new License("ShiZhong", "ZhangKe", License.APACHE_2, "https://github.com/0xZhangKe/ShiZhong"));
     }
 
     @Override
     public boolean onRecommendedClicked(@NonNull View itemView, @NonNull Recommended recommended) {
-        if(recommended.openWithGooglePlay) {
+        if (recommended.openWithGooglePlay) {
             openMarket(this, recommended.packageName, recommended.downloadUrl);
         } else {
             openWithBrowser(this, recommended.downloadUrl);
